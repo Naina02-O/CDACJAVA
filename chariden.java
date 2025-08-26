5890200b23954527a5b471512a8171bd39ca5545
Naina02-O <itznaina7@gmail.com>
2025-08-26 20:36:30 +0530

    Create chariden.java

---
+++ chariden.java
@@ -0,0 +1,9 @@
+import.java.util.Scanner;
+class simpleinterest{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+         string ch = sc.nextLine();
+
+         System.out.println("Enter the value");
+    }
+}
